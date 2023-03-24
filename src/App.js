1dfbd439296a5f873defaec33bbfcd58f2dc5e15
DevanshYadav0702 <devanshyadav0702@gmail.com>
2023-03-24 08:01:39 +0530

    add router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DisplayProduct from "./Components/DisplayProduct/DisplayProduct";
-
 import Login from "./Components/Login/Login";
-// import "./App.css";
-
+import Navbar from "./Components/Navbar/Navbar";
 import Product from './Components/Product/Product';
 
 function App() {
 
  
   return(
-    <div className=''>
-
-      <Login/>
-      {/* add product */}
+    <div className='mainApp'>
+      <Navbar/>
+      <div>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/Verify" element={<DisplayProduct/>}/>
+          <Route path="/Product" element={<Product/>}/>
+        </Routes>
+      </div>
+      {/* <Login/>
+      
       <Product/>
-      <DisplayProduct/>
+      <DisplayProduct/> */}
 
     </div>
   );
