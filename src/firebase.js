f44a51c883c315c14c2f348c0aef7039c678e547
DevanshYadav0702 <devanshyadav0702@gmail.com>
2023-03-25 03:56:23 +0530

    update fire

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
-import { getDatabase } from 'firebase/database'
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAwaZGKHtPMDcdtjHxwbeFt-qgj6E1a220",
-  authDomain: "extended-creek-380015.firebaseapp.com",
-  projectId: "extended-creek-380015",
-  storageBucket: "extended-creek-380015.appspot.com",
-  messagingSenderId: "890734925950",
-  appId: "1:890734925950:web:9d899bb9adc32fbc7ee02f",
-  measurementId: "G-CMMR2VDCYG"
+  apiKey: "AIzaSyCjgz3ZzKOsEYAsalmYZALY0ZaC2EXiBv0",
+  authDomain: "counterfeit-auth.firebaseapp.com",
+  projectId: "counterfeit-auth",
+  storageBucket: "counterfeit-auth.appspot.com",
+  messagingSenderId: "22129602915",
+  appId: "1:22129602915:web:9c04a3d9b124ffdf6a6356",
+  measurementId: "G-BZSTZ0S0N4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase();
-// console.log(db.email);
-console.log(app);
-const auth = getAuth();
+const analytics = getAnalytics(app);
+const auth=getAuth();
 
-export { app, auth, db };
+export {app,auth};
